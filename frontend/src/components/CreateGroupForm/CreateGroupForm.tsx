58be08fc6fcac4656f49af6cab8228137b54fc77
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-08-31 14:12:51 +0900

    mod:【検証】CreateGroupFormを更新

--- frontend/src/components/CreateGroupForm/CreateGroupForm.tsx
+++ frontend/src/components/CreateGroupForm/CreateGroupForm.tsx
@@ -1,5 +1,5 @@
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import {
   FormErrorMessage,
   FormLabel,
@@ -7,8 +7,8 @@ import {
   Input,
   Button,
   VStack,
-} from "@chakra-ui/react";
-import { groupSchema } from "../../schema/group";
+} from '@chakra-ui/react';
+import { groupSchema } from '../../schema/group';
 
 // フォームで使用する変数の型を定義
 export interface formInputs {
@@ -35,32 +35,32 @@ const CreateGroupForm: React.FC<CreateGroupFormProps> = ({ onSubmit }) => {
   const onSubmitForm = handleSubmit(async (data) => {
     try {
       await onSubmit(data);
-      console.info("登録成功");
+      console.info('登録成功');
       reset();
     } catch (e) {
       console.error(e);
-      window.alert("登録に失敗");
+      window.alert('登録に失敗');
     }
   });
 
   return (
-    <VStack maxW={"600px"}>
-      <form onSubmit={onSubmitForm} style={{ width: "60%" }}>
+    <VStack maxW={'600px'}>
+      <form onSubmit={onSubmitForm} style={{ width: '60%' }}>
         {/* 名前 */}
         <FormControl mb={5} isInvalid={Boolean(errors.name)}>
           <FormLabel htmlFor="name">グループ名</FormLabel>
-          <Input id="name" placeholder="旅行" {...register("name")} />
+          <Input id="name" placeholder="旅行" {...register('name')} />
           <FormErrorMessage>
             {errors.name && errors.name.message}
           </FormErrorMessage>
         </FormControl>
         {/* メールアドレス */}
         <FormControl mb={10} isInvalid={Boolean(errors.members)}>
-          <FormLabel htmlFor="members">メンバー</FormLabel>
+          <FormLabel htmlFor="members">メンバー名</FormLabel>
           <Input
             id="members"
             placeholder="太郎, 花子"
-            {...register("members")}
+            {...register('members')}
           />
           <FormErrorMessage>
             {errors.members && errors.members.message}
@@ -70,7 +70,7 @@ const CreateGroupForm: React.FC<CreateGroupFormProps> = ({ onSubmit }) => {
           colorScheme="green"
           isLoading={isSubmitting}
           type="submit"
-          w={"full"}
+          w={'full'}
         >
           グループを作成
         </Button>
